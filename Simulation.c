651cb1fc65e1001a60c2d38b7e4f85399944872b
Abdullah İbrahim Durusoy <abdullah.durusoy31@gmail.com>
2023-12-17 17:34:37 +0300

    asd

--- Simulation.c
+++ Simulation.c
@@ -3,23 +3,24 @@
 #include <complex.h>
 
 int main() {
-    double Rocket_W = 15;   // kg
-    double Fuel_W = 6;      // kg
+    double Rocket_Weight = 15;   // kg
+    double Fuel_Weight = 6;      // kg
     double Fuel_Burn = 3;   // kg/s
-    double g = 9.81;        // m/s^2
-    double Sum_W = Rocket_W + Fuel_W;
-    double time = Fuel_W / Fuel_Burn;
+    double gravity = 9.81;        // m/s^2
+    double Sum_W = Rocket_Weight + Fuel_Weight;
+    double time = Fuel_Weight / Fuel_Burn;
     double v_t[100 * (int)time][2];
     double t_W_r = 2;        // thrust weight ratio
-    double v_e = (g * Sum_W * t_W_r) / Fuel_Burn;   // m/s
+    double v_e = (gravity * Sum_W * t_W_r) / Fuel_Burn;   // m/s
     double S = 0;
     int k = 0;
 
-    // Open a file for writing
+    // Opening a file to save my data
     FILE *data = fopen("data.txt", "w");
+    // Open a file for GNUplot
 	FILE *plot = popen("gnuplot -persist", "w");
 
-
+    // Setting window options
 	fprintf(plot, "set term wxt persist\n");
 	fprintf(plot, "set title \"Velocity vs Time\"\n");
 
@@ -28,10 +29,11 @@ int main() {
         return 1;
     }
 
-    for (double t = 0; t <= time; t += 0.01) {
-        double Gravity_Velocity = g * t;
+    for (double t = 0; t <= time; t += 0.01)
+    {
+        double Gravity_Velocity = gravity * t;
     	double ln_term = log(Sum_W / (Sum_W * (1 - Fuel_Burn * t)));
-    	double v = v_e * (fabs(ln_term)) - Gravity_Velocity;
+    	double v = v_e * fabs(ln_term) - Gravity_Velocity;
 
         if (v >= v_e && k == 0)
             k = 1;
@@ -45,9 +47,9 @@ int main() {
 		fflush(data);
         S += v / 100;
     }
+    // Run GNUplot
 	fprintf(plot, "plot 'data.txt' using 1:2 with lines lw 3\n");
 	fflush(plot);
     fclose(data);
-
     return 0;
 }
