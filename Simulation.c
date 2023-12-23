8edf73f46eb40c7823c9bf315fce4751c7e98a00
Abdullah İbrahim Durusoy <99260593+marasama@users.noreply.github.com>
2023-12-23 03:08:24 +0300

    Add files via upload

--- Simulation.c
+++ Simulation.c
@@ -1,55 +1,43 @@
 #include <stdio.h>
-#include <math.h>
 #include <complex.h>
 
 int main() {
-    double Rocket_Weight = 15;   // kg
-    double Fuel_Weight = 6;      // kg
+    double Rocket_W = 15;   // kg
+    double Fuel_W = 6;      // kg
     double Fuel_Burn = 3;   // kg/s
-    double gravity = 9.81;        // m/s^2
-    double Sum_W = Rocket_Weight + Fuel_Weight;
-    double time = Fuel_Weight / Fuel_Burn;
-    double v_t[100 * (int)time][2];
-    double t_W_r = 2;        // thrust weight ratio
-    double v_e = (gravity * Sum_W * t_W_r) / Fuel_Burn;   // m/s
+    double g = 9.81;        // m/s^2
+    double Sum_W = Rocket_W + Fuel_W;
+    double time = Fuel_W / Fuel_Burn;
+    double thrust_weight_ratio = 2;        // thrust weight ratio
+    double v_e = (g * Sum_W * thrust_weight_ratio) / Fuel_Burn;   // m/s
     double S = 0;
     int k = 0;
 
-    // Opening a file to save my data
+    // Open a file for writing
     FILE *data = fopen("data.txt", "w");
-    // Open a file for GNUplot
-	FILE *plot = popen("gnuplot -persist", "w");
+    FILE *plot = popen("gnuplot -persist", "w");
 
-    // Setting window options
-	fprintf(plot, "set term wxt persist\n");
-	fprintf(plot, "set title \"Velocity vs Time\"\n");
-
-    if (data == NULL) {
-        fprintf(stderr, "Error opening output file.\n");
-        return 1;
-    }
-
-    for (double t = 0; t <= time; t += 0.01)
-    {
-        double Gravity_Velocity = gravity * t;
-    	double ln_term = log(Sum_W / (Sum_W * (1 - Fuel_Burn * t)));
-    	double v = v_e * fabs(ln_term) - Gravity_Velocity;
+    fprintf(plot, "set term wxt persist\n");
+    fprintf(plot, "set title \"Velocity vs Time\"\n");
+    for (double t = 0.01; t <= time; t += 0.01) {
+        double Gravity_Velocity = g * t;
+        // Use complex logarithm to handle negative or zero values
+        double complex ln_term = clog(Sum_W / (Sum_W * (1 - Fuel_Burn * t)));
+        double v = v_e * cabs(ln_term) - Gravity_Velocity;
 
         if (v >= v_e && k == 0)
             k = 1;
-		else
-		{
+        else {
             if (k != 1)
                 v = 0;
         }
         // Write data to the file
         fprintf(data, "%f %f\n", t, v);
-		fflush(data);
+        fflush(data);
         S += v / 100;
     }
-    // Run GNUplot
-	fprintf(plot, "plot 'data.txt' using 1:2 with lines lw 3\n");
-	fflush(plot);
+    fprintf(plot, "plot 'data.txt' using 1:2 with lines lw 3\n");
+    fflush(plot);
     fclose(data);
     return 0;
 }
